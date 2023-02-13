ef02f922371454a537c667a76d40886718e71ba5
robertrugina912 <70482705+robertrugina912@users.noreply.github.com>
2023-02-13 04:58:11 +0100

    Add files via upload

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Robert Rugina on 2023-01-10.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
